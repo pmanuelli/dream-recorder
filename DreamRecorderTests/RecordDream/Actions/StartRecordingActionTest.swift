8490d112b2befbd903281498bba51b64faa1eba4
Pablo Manuelli <pablo.manuelli@gmail.com>
2018-05-27 23:25:31 -0300

    Test refactoring

--- DreamRecorderTests/RecordDream/Actions/StartRecordingActionTest.swift
+++ DreamRecorderTests/RecordDream/Actions/StartRecordingActionTest.swift
@@ -7,11 +7,27 @@ class StartRecordingActionTest: XCTestCase {
     
     func testWhenActionIsExecutedThenStartRecordingIsCalled() {
         
-        let audioRecorder = SpyAudioRecorder()
-        let action = StartRecording()
+        let audioRecorder = givenAnAudioRecorder()
+        let action = givenAnAction()
         
+        whenActionIsExecuted(action: action, audioRecorder: audioRecorder)
+        
+        assertStartRecordingIsCalledOnce(audioRecorder)
+    }
+    
+    private func givenAnAudioRecorder() -> SpyAudioRecorder {
+        return SpyAudioRecorder()
+    }
+    
+    private func givenAnAction() -> StartRecording {
+        return StartRecording()
+    }
+    
+    private func whenActionIsExecuted(action: StartRecording, audioRecorder: AudioRecorder) {
         action.execute(audioRecorder: audioRecorder)
-        
+    }
+    
+    private func assertStartRecordingIsCalledOnce(_ audioRecorder: SpyAudioRecorder) {
         XCTAssertEqual(audioRecorder.startRecordingCalls, 1)
     }
 }
